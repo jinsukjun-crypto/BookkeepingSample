636b2ba290a03b8d1c7080ced0f790f0f04a6021
jinsukjun-crypto <jinsuk.jun@me.com>
2026-03-11 14:56:12 -0400

    Add Transaction model

---
+++ Models/Transaction.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BookkeepingSample.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Category { get; set; }
+    }
+}
